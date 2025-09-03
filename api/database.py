c908eb0d1ec5fed1590cb57b96c5f57030774a41
ainvis <109056582+ainvishal@users.noreply.github.com>
2025-09-03 22:22:55 +0530

    update vercel.json

--- api/database.py
+++ api/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./notes.db")  # Fallback to SQLite for local
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_Dx2HqXTkN9wG@ep-odd-moon-ad70wcei-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require", "sqlite:///./notes.db")  # Fallback to SQLite for local
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
