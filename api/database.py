c85ade6054c277edc852970d2cd4db7029d2e539
ainvis <109056582+ainvishal@users.noreply.github.com>
2025-09-04 18:42:03 +0530

    updated database.py

--- api/database.py
+++ api/database.py
@@ -3,8 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_Dx2HqXTkN9wG@ep-odd-moon-ad70wcei-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require", "sqlite:///./notes.db")  # Fallback to SQLite for local
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./notes.db")
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
